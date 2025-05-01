cac41ae3fb9f7c07d488f3319582d042c6bb67b4
lestday233 <106459514+lest-day@users.noreply.github.com>
2025-05-01 18:51:02 +0800

    更新 AllBox.vue

--- src/components/AllFunc/AllBox.vue
+++ src/components/AllFunc/AllBox.vue
@@ -3,7 +3,8 @@
     <n-tab-pane class="no-padding height--full" name="link" tab="快链">
       <ShortCut />
     </n-tab-pane>
-    <n-tab-pane name="note" tab="便签"> 即将完善 </n-tab-pane>
+    <n-tab-pane name="note" tab="便签">
+    </n-tab-pane>
     <n-tab-pane name="more" tab="待办"> 还能有啥呢 😢 </n-tab-pane>
   </n-tabs>
 </template>
@@ -11,6 +12,7 @@
 <script setup>
 import { NTabs, NTabPane } from "naive-ui";
 import ShortCut from "@/components/AllFunc/Box/ShortCut.vue";
+import Note from "@/components/AllFunc/Box/Note.vue";
 </script>
 <style>
 .height--full {
